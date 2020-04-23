7822d0857a01cfb18fced99eec3c0bdf85a3f3a2
hch987 <1933199695@qq.com>
2020-04-24 00:09:23 +0800

    4.22

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180224/MyReceiver.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180224/MyReceiver.java
@@ -1,5 +1,4 @@
-package cn.edu.sdwu.android02.classroom.sn170507180224;
-
+﻿package cn.edu.sdwu.android02.classroom.sn170507180224;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -8,10 +7,9 @@ import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
 
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        //接收广播时自动调用
+        //接收广播时自动调用本方法
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
         //获取广播内容
